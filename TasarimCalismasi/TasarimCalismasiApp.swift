1e14c52da26889c2cdc0efa884b7359869138443
Nuri TIRAŞ <nuritiras@gmail.com>
2024-07-20 12:17:18 +0300

    Initial Commit

---
+++ TasarimCalismasi/TasarimCalismasiApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasarimCalismasiApp.swift
+//  TasarimCalismasi
+//
+//  Created by Nuri TIRAŞ on 20.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct TasarimCalismasiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
